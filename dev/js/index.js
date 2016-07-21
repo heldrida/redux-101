5ec4a7f99a77fdb3c696c60be2387eeebad29df3
Helder <heldrida@gmail.com>
2016-07-21 23:00:30 +0100

    described the store object

--- dev/js/index.js
+++ dev/js/index.js
@@ -2,8 +2,11 @@ import 'babel-polyfill';
 import React from 'react';
 import ReactDOM from "react-dom";
 import { createStore } from 'redux';
+import allReducers from './reducers';
 
-const store = createStore();
+// the main store is a single object that
+// takes all combined reducer data objects
+const store = createStore(allReducers);
 
 ReactDOM.render(
 	<h1>Hello world!</h1>,
