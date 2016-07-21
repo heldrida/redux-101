42be98c7d3760c3814e4e33b9f0ab3571765ded9
Helder <heldrida@gmail.com>
2016-07-21 22:58:39 +0100

    created reducer users and main reducer combiner

---
+++ dev/js/reducers/index.js
@@ -0,0 +1,6 @@
+import { combineReducers } from 'redux';
+import userReducer from './reducer-users';
+
+const allReducers = combineReducers({
+	users: userReducer
+});
---
+++ dev/js/reducers/reducer-users.js
@@ -0,0 +1,31 @@
+export default function () {
+	return [{
+		id: 1,
+		first: "Lee",
+		last: "Jackson",
+		age: 43,
+		description: "A pro skateboarder from California",
+		thumbnail: "http://loremflickr.com/300/300"
+	}, {
+		id: 2,
+		first: "Math",
+		last: "Roberts",
+		age: 25,
+		description: "Graffiti writter in the hip-hop scene.",
+		thumbnail: "http://loremflickr.com/300/300"
+	}, {
+		id: 3,
+		first: "Rocky",
+		last: "Silvano",
+		age: 52,
+		description: "Sivano is an amazing Surf boarder",
+		thumbnail: "http://loremflickr.com/300/300"
+	}{
+		id: 4,
+		first: "Mike",
+		last: "Finn",
+		age: 35,
+		description: "Footballer by nature",
+		thumbnail: "http://loremflickr.com/300/300"
+	}]
+}
